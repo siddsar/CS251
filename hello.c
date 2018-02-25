ba5a80f93608a374a4beec8aa7a0c25feebf2b30
Siddhant <siddhantsarkar2404@gmail.com>
2018-02-25 21:14:53 +0530

    Changes Made

--- hello.c
+++ hello.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 void main(){
 printf("Helloworld!\n");
-microkernel_sendmsg("is more portable");
+printf("This must be a monolithic
+design\n");
+microkernel_sendmsg("is more
+portable");
 }
 void microkernel_sendmsg(char *a){
 printf("microkernel: %s\n", a);
+}
+void microkernel_getmsg(char *b){
+//TODO: getmsg feature
 }
